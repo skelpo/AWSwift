54ce8b6db4df66c5cd1e246f27a289bc4de731ac
Ralph Kuepper <ralph.kuepper@skelpo.com>
2019-04-26 15:33:35 -0400

    Swift 5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,23 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "AWSwift",
+    products: [
+        .library(
+            name: "AWSwift",
+            targets: ["AWSwift"]
+        ),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "AWSwift",
+            dependencies: ["CryptoSwift"]
+        ),
     ]
 )
